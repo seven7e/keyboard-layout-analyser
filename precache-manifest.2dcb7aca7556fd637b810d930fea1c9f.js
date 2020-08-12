85ddb39fa7d65c26a1a50e89164d56604d210a26
nanoix9 <nanoix9@gmail.com>
2020-08-12 20:34:34 +1200

    Updates

---
+++ precache-manifest.2dcb7aca7556fd637b810d930fea1c9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6b352c170964c3d40e79869dbe8223",
+    "url": "/keyboard-layout-analyser/index.html"
+  },
+  {
+    "revision": "85bb3c5a78687e8e4c5a",
+    "url": "/keyboard-layout-analyser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "278eb02413358c04a2d0",
+    "url": "/keyboard-layout-analyser/static/js/2.62376da8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keyboard-layout-analyser/static/js/2.62376da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bb3c5a78687e8e4c5a",
+    "url": "/keyboard-layout-analyser/static/js/main.1e119a58.chunk.js"
+  },
+  {
+    "revision": "b255775648958102c574",
+    "url": "/keyboard-layout-analyser/static/js/runtime-main.a7ef77d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keyboard-layout-analyser/static/media/logo.5d5d9eef.svg"
+  }
+]);
